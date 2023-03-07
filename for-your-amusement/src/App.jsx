2f3d3b807f4d31f14a8590f8dcd99b0b4bf0463c
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-08 08:58:18 +0900

    fix: router error

--- for-your-amusement/src/App.jsx
+++ for-your-amusement/src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserLogin from "./pages/UserLogin";
 import UserSignUp from "./pages/UserSignUp";
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/userlogin" element={<UserLogin />} />
         <Route path="/usersignup" element={<UserSignUp />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 export default App;
