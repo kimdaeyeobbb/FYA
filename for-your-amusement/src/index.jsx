663645f3e5a2d7c6078610d2f8488a62231f787d
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-08 11:04:05 +0900

    feat: add styled-components global style

--- for-your-amusement/src/index.jsx
+++ for-your-amusement/src/index.jsx
@@ -1,8 +1,44 @@
 import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
+import ReactDOM from "react-dom/client"; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import { theme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+
+    body{
+        font-family: Noto Sans CJK KR;
+    }
+
+  .ir {
+  position: absolute;
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+}
+
+  button{
+    border:0;
+    cursor: pointer;
+    background-color:transparent;
+  }
+  
+  a{
+    color:black;
+    text-decoration:none;
+  }
+`;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>
+);
