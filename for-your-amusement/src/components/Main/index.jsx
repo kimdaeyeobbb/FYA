6b3b058949fd94435065056c69cf982be411767a
jihyunglee <jhlee7125@gmail.com>
2023-03-11 22:05:41 +0900

    design: Home 페이지 안에 들어가는 게임 버튼 디자인 구현

--- for-your-amusement/src/components/Main/index.jsx
+++ for-your-amusement/src/components/Main/index.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import * as S from './style';
 
 const index = () => {
-  return <S.MainContainer>index</S.MainContainer>;
+  return (
+    <S.MainContainer>
+      <S.MainBtnsSection>
+        <S.MainGameTypeContainer>
+          <S.MainGameTypeBtn className='active' type='button'>
+            최신게임
+          </S.MainGameTypeBtn>
+          <S.MainGameTypeBtn type='button'>인기게임</S.MainGameTypeBtn>
+        </S.MainGameTypeContainer>
+        <S.MainRandomGameBtn type='button'>랜덤 게임뽑기</S.MainRandomGameBtn>
+      </S.MainBtnsSection>
+      <S.MainGameSection>게임들</S.MainGameSection>
+    </S.MainContainer>
+  );
 };
 
 export default index;
--- for-your-amusement/src/components/Main/style.jsx
+++ for-your-amusement/src/components/Main/style.jsx
@@ -7,3 +7,46 @@ export const MainContainer = styled.main`
   padding: 170px 155px 198px;
   background-color: red;
 `;
+
+export const MainBtnsSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const MainGameTypeContainer = styled.div``;
+
+export const MainGameTypeBtn = styled.button`
+  width: 180px;
+  height: 70px;
+  background: #e2e2e2 0% 0% no-repeat padding-box;
+  border-radius: 43px;
+  padding: 20px 51px;
+  font: normal normal normal 20px/29px Noto Sans CJK KR;
+  letter-spacing: 0;
+  color: #818181;
+
+  &.active {
+    background: #5ba7a7 0% 0% no-repeat padding-box;
+    color: #fff;
+  }
+
+  + button {
+    margin-left: 20px;
+  }
+`;
+
+export const MainRandomGameBtn = styled.button`
+  width: 220px;
+  height: 70px;
+  background: #6b8cbf 0% 0% no-repeat padding-box;
+  font: normal normal normal 20px/29px Noto Sans CJK KR;
+  letter-spacing: 0;
+  color: #fff;
+`;
+
+export const MainGameSection = styled.section`
+  margin-top: 68px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-basis: 1920px;
+`;
