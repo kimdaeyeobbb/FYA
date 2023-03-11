5ff27e86d858fed52c78b61b73294e1fd3085a92
jihyunglee <jhlee7125@gmail.com>
2023-03-12 03:42:06 +0900

    design: UserLogin 페이지 컴포넌트 디자인 구현

--- for-your-amusement/src/pages/UserLogin/index.jsx
+++ for-your-amusement/src/pages/UserLogin/index.jsx
@@ -1,46 +1,29 @@
-import React, { useState, useEffect } from "react";
-import fya from "../../assets/images/login_logo.svg";
-import * as S from "./style";
+import React, { useState, useEffect } from 'react';
+import InputBox from '../../components/InputBox';
+import Button from '../../components/Button';
+import MainLogo from '../../assets/images/login_logo.svg';
+import IdIcon from '../../assets/images/ID_icon.svg';
+import PwIcon from '../../assets/images/PW_icon.png';
+import * as S from './style';
+import { Link } from 'react-router-dom';
 
-export function LongBtn({ disabled = true, message }) {
-  return (
-    <>
-      {disabled ? (
-        <S.DisabledBtn>{message}</S.DisabledBtn>
-      ) : (
-        <S.AbledBtn>{message}</S.AbledBtn>
-      )}
-    </>
-  );
-}
+export default function UserLogin() {
+  const [userId, setUserId] = useState('');
+  const [userIdError, setUserIdError] = useState('');
+  const [userIdValid, setUserIdValid] = useState(false);
 
-export function InputBox({ type = "text", useRef, ...props }) {
-  return (
-    <>
-      <S.InputContainer>
-        <S.Input type={type} ref={useRef} {...props} />
-      </S.InputContainer>
-    </>
-  );
-}
-
-export default function UserLogin(props) {
-  const [userId, setUserId] = useState("");
-  const [userIdError, setUserIdError] = useState("");
-  const [userIdValid, setUserIdValid] = useState("");
-
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [passwordValid, setPasswordValid] = useState("");
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [passwordValid, setPasswordValid] = useState(false);
 
   useEffect(() => {
     const userIdRex = /^[a-z0-9]{6,20}/;
 
-    if (!userIdRex.test(userId) && userId !== "") {
-      setUserIdError("아이디는 6~20자 이내입니다.");
+    if (!userIdRex.test(userId) && userId === '') {
+      setUserIdError('아이디는 6~20자 이내입니다.');
       setUserIdValid(false);
     } else {
-      setUserIdError("");
+      setUserIdError('');
       setUserIdValid(true);
     }
   }, [userId]);
@@ -48,44 +31,58 @@ export default function UserLogin(props) {
   useEffect(() => {
     const passwordRex = /^[a-zA-Z0-9]{8,20}$/;
 
-    if (!passwordRex.test(passwordRex)) {
-      setPasswordError("비밀번호는 8~16자 이내입니다.");
+    if (!passwordRex.test(password)) {
+      setPasswordError('비밀번호는 8~16자 이내입니다.');
       setPasswordValid(false);
     } else {
-      setPasswordError("");
+      setPasswordError('');
       setPasswordValid(true);
     }
   }, [password]);
 
+  console.log(userId, password);
+  console.log(userIdValid, passwordValid);
+
   return (
     <>
       <S.FormContainer>
-        <S.PageTitle className="ir">FYA 회원가입 화면</S.PageTitle>
-        <S.TitleImg src={fya} alt="로고 이미지" />
+        <S.PageTitle className='ir'>FYA 회원가입 화면</S.PageTitle>
+        <Link to='/'>
+          <S.LogoImg src={MainLogo} alt='로고 이미지' />
+        </Link>
         <S.InputForm>
           <InputBox
-            id="userId"
-            placeholder="아이디"
+            id='userId'
+            type='text'
+            imgSrc={IdIcon}
+            imgAlt='아이디'
+            placeholder='아이디'
             onChange={(e) => {
               setUserId(e.target.value);
             }}
           />
-          <S.ErrMsg className="message">{userIdError}</S.ErrMsg>
+          {!userIdValid && (
+            <S.ErrMsg className='message'>{userIdError}</S.ErrMsg>
+          )}
           <InputBox
-            type="password"
-            id="password"
-            placeholder="비밀번호"
+            id='password'
+            type='password'
+            imgSrc={PwIcon}
+            imgAlt='비밀번호'
+            placeholder='비밀번호'
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
-          <S.ErrMsg className="message">{passwordError}</S.ErrMsg>
+          {!passwordValid && (
+            <S.ErrMsg className='message'>{passwordError}</S.ErrMsg>
+          )}
+          <Button
+            disabled={!userIdValid || !passwordValid ? true : false}
+            message='로그인'
+          ></Button>
         </S.InputForm>
-        <LongBtn
-          disabled={userIdValid && passwordValid ? false : true}
-          message="로그인"
-        ></LongBtn>
-        <S.signUp to="/usersignup">회원가입</S.signUp>
+        <S.SignupLink to='/usersignup'>회원가입</S.SignupLink>
       </S.FormContainer>
     </>
   );
--- for-your-amusement/src/pages/UserLogin/style.jsx
+++ for-your-amusement/src/pages/UserLogin/style.jsx
@@ -1,109 +1,42 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const FormContainer = styled.section`
+export const FormContainer = styled.article`
   display: flex;
-  align-items: center;
   flex-direction: column;
-  height: 100%;
-  width: 100%;
-  padding-top: 8vh;
-  /* margin-top: 30px; */
-  font-family: "Spoqa Han Sans Neo", "sans-serif";
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 100vh;
+  margin: auto;
 `;
 
 export const PageTitle = styled.h1``;
 
-export const TitleImg = styled.img`
-  background-image: url("../../assets/images/login_logo.svg");
+export const LogoImg = styled.img`
+  width: 326.15px;
+  height: 147.03px;
 `;
 
 export const InputForm = styled.form`
-  margin: 70px 0 30px;
+  margin: 70px 0 20px;
+  width: 100%;
 `;
 
 export const ErrMsg = styled.p`
-  margin-bottom: 10px;
-  color: red;
-`;
-
-export const DisabledBtn = styled.button`
-  /* background-color: ${(props) => props.theme.disabledColor};
-  color: #fff;
-  cursor: not-allowed;
-  display: block;
-  margin: 0 auto;
-  text-align: center;
-  width: 500px;
-  height: 70px;
-  border-radius: 20px;
-  font-family: "Spoqa Han Sans Neo";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 18px; */
-  /* Layout Properties */
-  /* Layout Properties */
-  top: 688px;
-  left: 710px;
-  width: 500px;
-  height: 70px;
-  /* UI Properties */
-  background: var(--unnamed-color-5ba7a7) 0% 0% no-repeat padding-box;
-  background: #5ba7a7 0% 0% no-repeat padding-box;
-  border-radius: 20px;
-  opacity: 1;
-  color: #fff;
-  font-size: 25px;
-  font: Noto Sans CJK KR;
-`;
-
-export const AbledBtn = styled(DisabledBtn)`
-  background-color: ${(props) => props.theme.mainColor};
-  color: #fff;
-  cursor: pointer;
-`;
-
-/* InputBox */
-
-export const InputContainer = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  width: 500px;
-  color: #767676;
-  font-weight: 500;
-`;
-
-export const Input = styled.input`
-  background-color: #f8f8f8;
-  border: none;
-  width: inherit;
-  padding-top: 16px;
-  margin-bottom: 16px;
-  height: 48px;
-  font-size: 14px;
-
-  &:focus {
-    outline: 0;
-    border: 2px solid #5ba7a7;
-  }
-
-  &::placeholder {
-    position: absolute;
-    bottom: 14px;
-    color: #dbdbdb;
-  }
+  font: normal normal 300 15px/22px Noto Sans CJK KR;
+  margin-left: 25px;
+  margin-bottom: 18px;
+  color: #ff0000;
 `;
 
 /* 회원가입 링크 */
-
-export const signUp = styled(Link)`
+export const SignupLink = styled(Link)`
   padding-top: 20px;
   color: #818181;
-  text-decoration: underline;
-  opacity: 1;
-  letter-spacing: 0px;
   font: normal normal 300 15px/22px Noto Sans CJK KR;
-  text-align: left;
+
+  :hover {
+    text-decoration: underline;
+  }
 `;
