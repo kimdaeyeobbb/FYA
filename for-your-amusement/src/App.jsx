47d35abbefbf7686b422ea839c3b7ac6f33a7dcb
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-11 20:35:47 +0900

    feat: 라우팅 설정

--- for-your-amusement/src/App.jsx
+++ for-your-amusement/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserLogin from "./pages/UserLogin";
 import UserSignUp from "./pages/UserSignUp";
+import ReactionVelocityCheck from "./pages/ReactionVelocityCheck";
+import LotteryMachine from "./pages/LotteryMachine";
+import RockPaperScissors from "./pages/rockPaperScissors";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/userlogin" element={<UserLogin />} />
         <Route path="/usersignup" element={<UserSignUp />} />
+        <Route path="/reactionvelocitycheck" element={ReactionVelocityCheck} />
+        <Route path="/rockpaperscissors" element={<RockPaperScissors />} />
+        <Route path="/lotterymachine" element={LotteryMachine} />
       </Routes>
     </Router>
   );
---
+++ for-your-amusement/src/pages/ReactionVelocityCheck/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReactionVelocityCheck() {
+  return <div>ReactionVelocityCheck</div>;
+}
