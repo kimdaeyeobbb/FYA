570d0e4b91ee7e2fac576c8ea0932afc752795c9
jihyunglee <jhlee7125@gmail.com>
2023-03-11 23:21:17 +0900

    fix: 컴포넌트 파일명은 대문자로 시작하여야 하기 때문에 파일명을 대문자로 변경함

--- for-your-amusement/src/App.jsx
+++ for-your-amusement/src/App.jsx
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import UserLogin from "./pages/UserLogin";
-import UserSignUp from "./pages/UserSignUp";
-import ReactionVelocityCheck from "./pages/ReactionVelocityCheck";
-import LotteryMachine from "./pages/LotteryMachine";
-import RockPaperScissors from "./pages/RockPaperScissors";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Index';
+import UserLogin from './pages/UserLogin';
+import UserSignUp from './pages/UserSignUp';
+import ReactionVelocityCheck from './pages/ReactionVelocityCheck';
+import LotteryMachine from './pages/LotteryMachine';
+import RockPaperScissors from './pages/RockPaperScissors';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/userlogin" element={<UserLogin />} />
-        <Route path="/usersignup" element={<UserSignUp />} />
-        <Route path="/reactionvelocitycheck" element={<ReactionVelocityCheck/>} />
-        <Route path="/rockpaperscissors" element={<RockPaperScissors />} />
-        <Route path="/lotterymachine" element={<LotteryMachine/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/userlogin' element={<UserLogin />} />
+        <Route path='/usersignup' element={<UserSignUp />} />
+        <Route
+          path='/reactionvelocitycheck'
+          element={<ReactionVelocityCheck />}
+        />
+        <Route path='/rockpaperscissors' element={<RockPaperScissors />} />
+        <Route path='/lotterymachine' element={<LotteryMachine />} />
       </Routes>
     </Router>
   );
