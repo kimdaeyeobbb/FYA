78a8f58d905d92b28bbe4f2d3eeb6db11d813ce6
jihyunglee <jhlee7125@gmail.com>
2023-03-12 01:10:01 +0900

    style: Footer import 수정

--- for-your-amusement/src/pages/Home/index.jsx
+++ for-your-amusement/src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header';
 import Main from '../../components/Main';
-import Footer from '../../components/Footer/Index';
+import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
