e3603a8cd2cb0be0cc5e8d6981f7e5557505e82d
jihyunglee <jhlee7125@gmail.com>
2023-03-11 21:33:15 +0900

    style: code format 스타일 수정

--- for-your-amusement/src/index.jsx
+++ for-your-amusement/src/index.jsx
@@ -1,40 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; //구버전
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import reset from "styled-reset";
-import { theme } from "./theme";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import reset from 'styled-reset';
+import { theme } from './theme';
 
 const GlobalStyle = createGlobalStyle`
-    ${reset}
+  ${reset}
 
-    body{
-        font-family: Noto Sans CJK KR;
-    }
+  body {
+    font-family: Noto Sans CJK KR;
+  }
 
   .ir {
-  position: absolute;
-  clip-path: inset(50%);
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-}
+    position: absolute;
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+  }
 
-  button{
+  button {
     border:0;
     cursor: pointer;
     background-color:transparent;
   }
   
-  a{
+  a {
     color:black;
     text-decoration:none;
   }
 `;
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <ThemeProvider theme={theme}>
