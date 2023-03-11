ba9df2e563894152912f2609734e7f6a0af5eaca
jihyunglee <jhlee7125@gmail.com>
2023-03-12 01:14:12 +0900

    design: Header userIcon, LogoImg Link 연결

--- for-your-amusement/src/components/Header/index.jsx
+++ for-your-amusement/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './style';
 import LogoImg from '../../assets/images/main_logo.svg';
 import UserImg from '../../assets/images/person_icon.svg';
@@ -6,8 +6,12 @@ import UserImg from '../../assets/images/person_icon.svg';
 export default function Header() {
   return (
     <S.HeaderWrapper>
-      <S.LogoImage src={LogoImg} alt='For Your Amusement' />
-      <S.UserIcon src={UserImg} alt='사용자 아이콘' />
+      <Link to='/'>
+        <S.LogoImage src={LogoImg} alt='For Your Amusement' />
+      </Link>
+      <Link to='/userlogin'>
+        <S.UserIcon src={UserImg} alt='사용자 아이콘' />
+      </Link>
     </S.HeaderWrapper>
   );
 }
