b3b0e86fdbfb003ddbc9551dbbb67e4bae827903
jihyunglee <jhlee7125@gmail.com>
2023-03-12 03:42:39 +0900

    design: UserSignUp 페이지 컴포넌트 디자인 구현

--- for-your-amusement/src/pages/UserSignUp/index.jsx
+++ for-your-amusement/src/pages/UserSignUp/index.jsx
@@ -1,46 +1,34 @@
-import React, { useState, useEffect } from "react";
-import fya from "../../assets/images/login_logo.svg";
-import * as S from "./style";
+import React, { useState, useEffect } from 'react';
+import InputBox from '../../components/InputBox';
+import Button from '../../components/Button';
+import MainLogo from '../../assets/images/login_logo.svg';
+import IdIcon from '../../assets/images/ID_icon.svg';
+import NameIcon from '../../assets/images/name_icon.svg';
+import PwIcon from '../../assets/images/PW_icon.png';
+import * as S from './style';
+import { Link } from 'react-router-dom';
 
-export function LongBtn({ disabled = true, message }) {
-  return (
-    <>
-      {disabled ? (
-        <S.DisabledBtn>{message}</S.DisabledBtn>
-      ) : (
-        <S.AbledBtn>{message}</S.AbledBtn>
-      )}
-    </>
-  );
-}
+export default function UserSignUp() {
+  const [userId, setUserId] = useState('');
+  const [userIdError, setUserIdError] = useState('');
+  const [userIdValid, setUserIdValid] = useState(false);
 
-export function InputBox({ type = "text", useRef, ...props }) {
-  return (
-    <>
-      <S.InputContainer>
-        <S.Input type={type} ref={useRef} {...props} />
-      </S.InputContainer>
-    </>
-  );
-}
+  const [name, setName] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [nameValid, setNameValid] = useState(false);
 
-export default function UserSignUp(props) {
-  const [userId, setUserId] = useState("");
-  const [userIdError, setUserIdError] = useState("");
-  const [userIdValid, setUserIdValid] = useState("");
-
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [passwordValid, setPasswordValid] = useState("");
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [passwordValid, setPasswordValid] = useState(false);
 
   useEffect(() => {
     const userIdRex = /^[a-z0-9]{6,20}/;
 
-    if (!userIdRex.test(userId) && userId !== "") {
-      setUserIdError("6~20자의 영문 소문자+숫자만 사용 가능합니다.");
+    if (!userIdRex.test(userId) && userId !== '') {
+      setUserIdError('6~20자의 영문 소문자+숫자만 사용 가능합니다.');
       setUserIdValid(false);
     } else {
-      setUserIdError("");
+      setUserIdError('');
       setUserIdValid(true);
     }
   }, [userId]);
@@ -51,49 +39,76 @@ export default function UserSignUp(props) {
 
     if (!passwordRex.test(passwordRex)) {
       setPasswordError(
-        "8~16자의 영문 대 소문자, 숫자, 특수문자만 사용 가능합니다."
+        '8~16자의 영문 대 소문자, 숫자, 특수문자만 사용 가능합니다.'
       );
       setPasswordValid(false);
     } else {
-      setPasswordError("");
+      setPasswordError('');
       setPasswordValid(true);
     }
   }, [password]);
 
   return (
     <S.FormContainer>
-      <S.PageTitle className="ir">FYA 회원가입 화면</S.PageTitle>
-      <S.TitleImg src={fya} alt="로고 이미지" />
+      <S.PageTitle className='ir'>FYA 회원가입 화면</S.PageTitle>
+      <Link to='/'>
+        <S.LogoImg src={MainLogo} alt='로고 이미지' />
+      </Link>
       <S.InputForm>
         <InputBox
-          id="userId"
-          placeholder="아이디"
+          id='userId'
+          type='text'
+          imgSrc={IdIcon}
+          imgAlt='아이디'
+          placeholder='아이디'
           onChange={(e) => {
             setUserId(e.target.value);
           }}
         />
+        {!userIdValid && <S.ErrMsg className='message'>{userIdError}</S.ErrMsg>}
         <InputBox
-          id="userId"
-          placeholder="아이디"
+          id='nickname'
+          type='text'
+          imgSrc={NameIcon}
+          imgAlt='닉네임'
+          placeholder='닉네임'
           onChange={(e) => {
-            setUserId(e.target.value);
+            setName(e.target.value);
           }}
         />
-        <S.ErrMsg className="message">{userIdError}</S.ErrMsg>
+        {!nameValid && <S.ErrMsg className='message'>{nameError}</S.ErrMsg>}
         <InputBox
-          type="password"
-          id="password"
-          placeholder="비밀번호를 설정해주세요"
+          id='password'
+          type='password'
+          imgSrc={PwIcon}
+          imgAlt='비밀번호'
+          placeholder='비밀번호'
           onChange={(e) => {
             setPassword(e.target.value);
           }}
         />
-        <S.ErrMsg className="message">{passwordError}</S.ErrMsg>
+        {!passwordValid && (
+          <S.ErrMsg className='message'>{passwordError}</S.ErrMsg>
+        )}
+        <InputBox
+          id='passwordCheck'
+          type='password'
+          imgSrc={PwIcon}
+          imgAlt='비밀번호 확인'
+          placeholder='비밀번호 확인'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        {!passwordValid && (
+          <S.ErrMsg className='message'>{passwordError}</S.ErrMsg>
+        )}
+        <Button
+          disabled={!userIdValid || !passwordValid ? true : false}
+          message='회원가입'
+        ></Button>
       </S.InputForm>
-      <LongBtn
-        disabled={userIdValid && passwordValid ? false : true}
-        message="회원가입"
-      ></LongBtn>
+      <S.LoginLink to='/userlogin'>로그인</S.LoginLink>
     </S.FormContainer>
   );
 }
