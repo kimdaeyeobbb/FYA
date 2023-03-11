7693551e8eaa6cd82034e9f6cc0b9af87193c4b3
jihyunglee <jhlee7125@gmail.com>
2023-03-11 21:07:11 +0900

    style: Header 디자인 구현

--- for-your-amusement/src/components/Header/index.jsx
+++ for-your-amusement/src/components/Header/index.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import * as S from "./style";
-import LogoImg from "../../assets/images/main_logo.svg";
-import { ImageLogo } from "./style";
+import React from 'react';
+import * as S from './style';
+import LogoImg from '../../assets/images/main_logo.svg';
+import UserImg from '../../assets/images/person_icon.svg';
 
 export default function Header() {
   return (
-    <S.HeaderContainer>
-      <S.HeaderWrapper>
-        <S.ImageLogo src={LogoImg} alt="ForYourAmusement" />
-      </S.HeaderWrapper>
-    </S.HeaderContainer>
+    <S.HeaderWrapper>
+      <S.LogoImage src={LogoImg} alt='For Your Amusement' />
+      <S.UserIcon src={UserImg} alt='사용자 아이콘' />
+    </S.HeaderWrapper>
   );
 }
--- for-your-amusement/src/components/Header/style.jsx
+++ for-your-amusement/src/components/Header/style.jsx
@@ -1,51 +1,22 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-export const HeaderContainer = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  background-color: #fff;
-  z-index: 10;
-`;
+import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
-  position: relative;
-  display: flex;
-  align-items: center;
-  max-width: 100%;
-  height: 48px;
-  padding: 0 16px;
+  padding: 39.01px 155px 34.69px;
+  width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
   border-bottom: 0.5px solid rgb(219, 219, 219);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-export const ImageLogo = styled.image`
-  width: 50px;
-  height: 50px;
-  //
+export const LogoImage = styled.img`
+  width: 235.8px;
+  height: 106.3px;
 `;
 
-// // SearchBtn
-// export const SearchBtn = styled(Link)`
-//   width: 24px;
-//   height: 24px;
-//   background-image: url("../../assets/images/person_icon.svg")
-//   margin-left: auto;
-// `;
-
-// export const SearchInp = styled.input`
-//   padding: 7px 16px;
-//   margin-left: auto;
-//   width: 100%;
-//   background: #f2f2f2;
-//   border-radius: 32px;
-//   font-size: 14px;
-//   border: 0;
-// `;
-// // HeaderTitle
-// export const HeaderTit = styled.h1`
-//   font-size: 18px;
-//   font-weight: 500;
-//   margin-top: 2px;
-// `;
+export const UserIcon = styled.img`
+  width: 35px;
+  height: 37.74px;
+`;
