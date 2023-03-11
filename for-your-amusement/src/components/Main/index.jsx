55d0d8cf3447266eefd35b4fc7a6304c7bee3fa8
jihyunglee <jhlee7125@gmail.com>
2023-03-11 21:34:36 +0900

    design: Main 컴포넌트 생성

---
+++ for-your-amusement/src/components/Main/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './style';
+
+const index = () => {
+  return <S.MainContainer>index</S.MainContainer>;
+};
+
+export default index;
---
+++ for-your-amusement/src/components/Main/style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.main`
+  width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+  padding: 170px 155px 198px;
+  background-color: red;
+`;
