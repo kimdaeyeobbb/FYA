7bcd0ede3a7fa1b27c59b440b91d6f3293286246
jihyunglee <jhlee7125@gmail.com>
2023-03-12 21:18:43 +0900

    design: GameLayout 디자인 구현

---
+++ for-your-amusement/src/components/GameLayout/index.jsx
@@ -0,0 +1,5 @@
+import * as S from './style';
+
+export default function GameLayout(props) {
+  return <S.GameLayout>{props.children}</S.GameLayout>;
+}
---
+++ for-your-amusement/src/components/GameLayout/style.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const GameLayout = styled.main`
+  box-sizing: inherit;
+  width: 100%;
+  margin: 0 auto;
+  padding: 170px 155px 164px;
+`;
