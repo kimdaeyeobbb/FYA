822477cd55ce60e144dbce1c4220ca8861b4f9e0
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-12 17:48:47 +0900

    feat: 스타일 추가

---
+++ for-your-amusement/src/pages/LotteryMachine/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const BallDetail = styled.div`
+  display: inline-block;
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  text-align: center;
+  margin-right: 46px;
+  margin-bottom: 46px;
+`;
---
+++ for-your-amusement/src/pages/RockPaperScissors/style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ComRSP = styled.div`
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
+`
+
