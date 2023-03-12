f3532ccc842229f42a3bfd46ae4e5c7b5269dc81
jihyunglee <jhlee7125@gmail.com>
2023-03-12 21:57:48 +0900

    design: GameCommon 디자인 구현

--- for-your-amusement/src/components/GameCommon/index.jsx
+++ for-your-amusement/src/components/GameCommon/index.jsx
@@ -2,14 +2,23 @@ import GameBtn from '../GameBtn';
 import * as S from './style';
 import LikeImg from '../../assets/images/like_icon.svg';
 import ShareImg from '../../assets/images/share_icon.svg';
+import ArrowImg from '../../assets/images/arrow_icon.svg';
 
-export default function GameCommon() {
+export default function GameCommon({ gameDesc }) {
   return (
     <S.CommonSection>
       <S.ButtonContainer>
         <GameBtn imgSrc={LikeImg} imgAlt='좋아요' />
         <GameBtn imgSrc={ShareImg} imgAlt='공유' />
       </S.ButtonContainer>
+      <S.GameDetail>
+        <S.GameDetailTitle>게임 설명</S.GameDetailTitle>
+        <S.GameDetailDesc>{gameDesc}</S.GameDetailDesc>
+      </S.GameDetail>
+      <S.GameMore>
+        <S.GameMoreText>게임 전체보기</S.GameMoreText>
+        <S.GameMoreImg src={ArrowImg} alt='' />
+      </S.GameMore>
     </S.CommonSection>
   );
 }
--- for-your-amusement/src/components/GameCommon/style.jsx
+++ for-your-amusement/src/components/GameCommon/style.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const CommonSection = styled.section`
   width: inherit;
@@ -9,4 +10,43 @@ export const ButtonContainer = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: end;
+  margin-bottom: 182px;
+`;
+
+export const GameDetail = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  height: 305px;
+`;
+
+export const GameDetailTitle = styled.strong`
+  display: inline-block;
+  font: normal normal bold 20px/30px Noto Sans CJK KR;
+  color: #000;
+`;
+
+export const GameDetailDesc = styled.p`
+  margin-top: 30px;
+  max-width: 700px;
+  font: normal normal normal 20px/30px Noto Sans CJK KR;
+  word-break: normal;
+`;
+
+export const GameMore = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 120px;
+`;
+
+export const GameMoreText = styled.strong`
+  font: normal normal bold 30px/45px Noto Sans CJK KR;
+  margin-right: 18px;
+`;
+
+export const GameMoreImg = styled.img`
+  width: 18.53px;
+  height: 30.89px;
 `;
