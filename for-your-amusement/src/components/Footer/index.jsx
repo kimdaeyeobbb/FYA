d1c3e14ba5c2e918521a5d88955f9d8d512e8873
DaeYeob Kim <kimdyk2@hanmail.net>
2023-03-15 02:24:12 +0900

    fix: footer 디자인 시안에 맞게 수정

--- for-your-amusement/src/components/Footer/index.jsx
+++ for-your-amusement/src/components/Footer/index.jsx
@@ -9,12 +9,12 @@ export default function Footer() {
       </S.FooterTop>
       <S.FooterBottom>
         <S.FooterDictList>
-          <S.FooterDictTitle>상호</S.FooterDictTitle>
-          <S.FooterDictDefinition>
-            FYA(For Your Amusement·Action)
-          </S.FooterDictDefinition>
-          <S.FooterDictTitle>제작자</S.FooterDictTitle>
-          <S.FooterDictDefinition>팀명</S.FooterDictDefinition>
+          {/*<S.FooterDictTitle>상호</S.FooterDictTitle>*/}
+          {/*<S.FooterDictDefinition>*/}
+          {/*  FYA(For Your Amusement·Action)*/}
+          {/*</S.FooterDictDefinition>*/}
+          {/*<S.FooterDictTitle>제작자</S.FooterDictTitle>*/}
+          {/*<S.FooterDictDefinition>팀명</S.FooterDictDefinition>*/}
         </S.FooterDictList>
         <S.FooterDictList>
           <S.FooterDictTitle className='ir'>저작권</S.FooterDictTitle>
