1def26a62e6b24014b43b21fc4d576bd21fc4343
jihyunglee <jhlee7125@gmail.com>
2023-03-15 00:28:22 +0900

    design: 반응속도체크 스크린을 position absolute로 설정함에 따라 GameLayout 컴포넌트에서 잡아주는 relative를 설정함

--- for-your-amusement/src/components/GameLayout/style.jsx
+++ for-your-amusement/src/components/GameLayout/style.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const GameLayout = styled.main`
   box-sizing: inherit;
+  position: relative;
   width: 100%;
   margin: 0 auto;
   padding: 170px 155px 164px;
