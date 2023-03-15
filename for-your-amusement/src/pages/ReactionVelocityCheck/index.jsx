7e52638b5678336a054507670afe8197193aaf9e
jihyunglee <jhlee7125@gmail.com>
2023-03-16 01:51:32 +0900

    design: state가 waiting일 때 TargetCircle의 display none 처리

--- for-your-amusement/src/pages/ReactionVelocityCheck/index.jsx
+++ for-your-amusement/src/pages/ReactionVelocityCheck/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useContext } from 'react';
+import { useState, useRef, useContext, useEffect } from 'react';
 import GameHeader from '../../components/GameHeader';
 import GameCommon from '../../components/GameCommon';
 import GameLayout from '../../components/GameLayout';
@@ -16,19 +16,30 @@ export default function ReactionVelocityCheck() {
   const timeout = useRef(null);
   const startTime = useRef();
   const endTime = useRef();
+  const targetCircle = useRef();
 
-  const [backGroundColor, setBackGroundColor] = useState("#5BA7A7");
+  const [backGroundColor, setBackGroundColor] = useState('#5BA7A7');
+
+  useEffect(() => {
+    if (state === 'waiting') {
+      targetCircle.current.style.display = 'none';
+    } else {
+      targetCircle.current.style.display = 'block';
+    }
+    console.log(state);
+    console.log(targetCircle.current.style['backgroundColor']);
+  }, [state]);
 
   const onClickScreen = () => {
     if (state === 'waiting') {
       setState('ready');
       setMessage('노란색이 되면 클릭하세요.');
-      setBackGroundColor("#D21818");
+      setBackGroundColor('#D21818');
       // useRef는 내부에 current가 있으므로 current에 넣어줘야 함
       timeout.current = setTimeout(() => {
         setState('now');
         setMessage('지금!');
-        setBackGroundColor("#EAD977");
+        setBackGroundColor('#EAD977');
         startTime.current = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
     } else if (state === 'ready') {
@@ -36,7 +47,7 @@ export default function ReactionVelocityCheck() {
       clearTimeout(timeout.current);
       setState('waiting');
       setMessage('너무 성급하시군요! 노란색이 된 후에 클릭하세요.');
-      setBackGroundColor("#EAD977");
+      setBackGroundColor('#EAD977');
     } else if (state === 'now') {
       // 반응속도 체크
       endTime.current = new Date();
@@ -44,7 +55,7 @@ export default function ReactionVelocityCheck() {
       // setMessage('클릭해서 시작하세요.');
       setMessage('시작하시려면 현재 화면을 클릭하세요.');
       // setMessage('시작하시려면 현재 화면을 클릭하세요.');
-      setBackGroundColor("#5BA7A7")
+      setBackGroundColor('#5BA7A7');
       setResult((prevResult) => {
         return [...prevResult, endTime.current - startTime.current];
       }); // 예전 state를 참고하므로 함수형으로 써줄 것
@@ -57,10 +68,11 @@ export default function ReactionVelocityCheck() {
 
   // 지형: 게임이 끝난 뒤 평균 반응속도에 대한 결과값은 message로 나타내는 것이 좋을 것 같습니다
   const renderAverage = () => {
-    return result.length === 0 ? null : (   // JSX상 null은 태그가 없는 것
+    return result.length === 0 ? null : ( // JSX상 null은 태그가 없는 것
       <>
         <div>
-          {(result.reduce((acc, cur) => acc + cur) / result.length).toFixed(2)} MS
+          {(result.reduce((acc, cur) => acc + cur) / result.length).toFixed(2)}{' '}
+          MS
         </div>
         {/*<button onClick={onReset}>리셋</button>*/}
       </>
@@ -79,10 +91,13 @@ export default function ReactionVelocityCheck() {
           <S.GameMessage>{message}</S.GameMessage>
           {/* TargetCircle은 게임 시작과 끝 사이에만 렌더링이 되도록 조건부 렌더링을 구현하시는 것이 좋을 것 같습니다 */}
           {/* 만일 조건부 렌더링을 하였음에도 불구하고 게임이 시작되기 이전에 레이아웃을 잡아먹는다면 display: none을 시작과 끝에 적용하세요  */}
-          <S.TargetCircle style={{background:backGroundColor}}></S.TargetCircle>
+          <S.TargetCircle
+            ref={targetCircle}
+            style={{ background: backGroundColor }}
+          ></S.TargetCircle>
           {/*<S.TargetCircle></S.TargetCircle>*/}
-          <div style={{fontSize:80, fontWeight:"bold", color:"#FFF"}}>
-      {renderAverage()}
+          <div style={{ fontSize: 80, fontWeight: 'bold', color: '#FFF' }}>
+            {renderAverage()}
           </div>
         </S.GameScreen>
       </S.GameMain>
